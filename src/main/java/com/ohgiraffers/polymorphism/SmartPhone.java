565a600eecc27f632400f257a3ce5fdd0848a437
rlawlsdyd5932 <157683550+rlawlsdyd5932@users.noreply.github.com>
2024-02-13 13:13:17 +0900

    Update SmartPhone.java

--- src/main/java/com/ohgiraffers/polymorphism/SmartPhone.java
+++ src/main/java/com/ohgiraffers/polymorphism/SmartPhone.java
@@ -5,6 +5,12 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class SmartPhone {
+    스마트폰(부모 클래스) 삼성&애플(자식 클래스)
+
+스마트폰(부모 클래스) 
+    기능(스마트폰 클래스에서 기능으로 오버라이딩한다) 
+    -스마트폰의 기능 -안면인식 (각 핸드폰의 안면인식 프로그램 작성) 
+    -건강 모니터링 (각 핸드폰의 건강 모니터링 프로그램 작성)
 
     public void setOn() {}
 
